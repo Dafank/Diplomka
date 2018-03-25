cf27e178fa8e5d9f273e3d279d351064bf36b8b0
Dafank <fentik9814@gmail.com>
2018-03-25 17:59:02 +0300

    Pattern

---
+++ Diplomka/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diplomka.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Translate()
+        {
+            return View();
+        }
+
+        public ActionResult Grammar()
+        {
+            return View();
+        }
+    }
+}
