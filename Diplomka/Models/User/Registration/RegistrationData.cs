ee482b30171b8a38336dc4539f888ae50009c76d
Dafank <fentik9814@gmail.com>
2018-04-03 00:17:19 +0300

    added registration form and added new bugs

--- Diplomka/Models/User/Registration/RegistrationData.cs
+++ Diplomka/Models/User/Registration/RegistrationData.cs
@@ -12,16 +12,13 @@ namespace Diplomka.Models.User.Registration
         [RegularExpression(@"(?i)\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b", ErrorMessage = "Email address is not correct")]
         public string Email { get; set; }
 
-        [Display(Name = "Nickname:")]
-        [StringLength(15,MinimumLength = 6, ErrorMessage = "You must specify a nickname between 6 and 15 characters")]
-        public string NickName { get; set; }
-
         [Required(ErrorMessage = "You have not entered the password")]
         [StringLength(20, MinimumLength = 5, ErrorMessage = "You must specify a password between 5 and 20 characters")]
         [Compare("PasswordConfirm", ErrorMessage = "Passwords do not match")]
         public string Password { get; set; }
 
         [Display(Name = "Confirm password:")]
+        [Required(ErrorMessage = "You have not entered confirm password")]
         public string PasswordConfirm { get; set; }
     }
 }
